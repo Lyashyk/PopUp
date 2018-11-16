074a48d78fc9c598cea08c7a2665f849ba32b110
aleks <aleks1002x@gmail.com>
2018-11-16 15:25:51 +0200

    Update scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,11 @@ const popupCallerButton = document.querySelectorAll(".button")[0];
 const dialog = document.querySelector(".dialog");
 const dialogContent = document.querySelector(".dialog-content");
 const popUp = document.querySelector(".popUp");
+const popupCloseButton = document.getElementsByClassName("buttonIcon")[0];
 
 popupCallerButton.onclick = () => {
   dialog.classList.add("dialog--visible");
 };
-
-const popupCloseButton = document.getElementsByClassName("buttonIcon")[0];
 
 popupCloseButton.onclick = () => {
   dialog.classList.remove("dialog--visible");
@@ -18,23 +17,11 @@ dialog.onclick = e => {
     dialog.classList.remove("dialog--visible");
   }
 };
-
+//-------------------------------------------------------------------------------------------------------------------------------------------
 const emailValidate = document.getElementsByClassName("input-field")[0];
 const input = document.getElementsByClassName("input")[0];
 const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-// emailValidate.onchange = () => {
-//   if (emailValidate.value != reg) {
-//     input.classList.add("input--fieldError");
-//   } else {
-//     input.classList.remove("input--fieldError");
-//   }
-// };
-
-// if (reg.test(address) == false) {
-//   alert("Введите корректный e-mail");
-//   return false;
-// }
+popupValidButton = document.getElementsByClassName("button")[1];
 
 emailValidate.onchange = () => {
   if (reg.test(emailValidate.value) == false) {
@@ -43,9 +30,6 @@ emailValidate.onchange = () => {
     input.classList.remove("input--fieldError");
   }
 };
-
-popupValidButton = document.getElementsByClassName("button")[1];
-console.log(popupValidButton);
 
 popupValidButton.onclick = () => {
   if (reg.test(emailValidate.value) == true) {
